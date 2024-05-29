774b155e4985fc7d9a85b13f8f44b3ebd202e975
Luka Vukovic <vukovicluka33@gmail.com>
2024-05-29 17:01:26 +0200

    First commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+const mysql = require('mysql2');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const port = process.env.PORT || 8080; 
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  port: process.env.DB_PORT || 3306,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/api/items', (req, res) => {
+  pool.query('SELECT * FROM users', (error, results) => {
+    if (error) {
+      return res.status(500).send(error);
+    }
+    res.json(results);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
