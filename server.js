85fad3d8f44106fe5d1d5e52207badceddc12472
Luka Vukovic <vukovicluka33@gmail.com>
2024-05-29 17:12:30 +0200

    Test

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-require('dotenv').config();
+require('dotenv').config({path: `.env.${app.get('env')}`});
 
 const mysql = require('mysql2');
 const cors = require('cors');
