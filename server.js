bf9d324aa2e0bfd6f61c68812cec174196460ef4
Luka Vukovic <vukovicluka33@gmail.com>
2024-05-29 17:21:16 +0200

    Test

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const app = express();
-require('dotenv').config({path: `.env.${app.get('env')}`});
-
+require('dotenv').config( {path: `.env.${app.get('env')}`});
 const mysql = require('mysql2');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path');
 
-const port = process.env.PORT || 8080; 
+const port = process.env.PORT;
+
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  port: process.env.DB_PORT || 3306,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: process.env.HOST,
+  user: process.env.USER,
+  port: process.env.PORT,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
@@ -22,6 +23,8 @@ const pool = mysql.createPool({
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, '../Angular-Quizzes/dist/Angular-Quizzes/browser')));
+
 app.get('/api/items', (req, res) => {
   pool.query('SELECT * FROM users', (error, results) => {
     if (error) {
@@ -29,6 +32,10 @@ app.get('/api/items', (req, res) => {
     }
     res.json(results);
   });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../Angular-Quizzes/dist/Angular-Quizzes/browser/index.html'));
 });
 
 app.listen(port, () => {
